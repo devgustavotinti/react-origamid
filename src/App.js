ec9530e6a31d7ad148be31d0619cee8b1df24660
Gustavo Tinti <devgustavotinti@gmail.com>
2021-05-20 19:50:28 -0300

    Hooks - funcoes com efeitos colaterais no StrictMode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import ButtonModal from "./ButtonModal";
-import Modal from "./Modal";
+
 
 const App = () => {
- const [modal, setModal] = React.useState(true)
+  const [contar, setContar] = React.useState(1)
+  const [itens, setItens] = React.useState(['Item 1'])
+
+  function handleClick() {
+    setContar((contar) => contar + 1)
+    setItens((itens) => [...itens, 'Item ' + (contar + 1)])
+  }
 
   return (
     <div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      {itens.map((item) => <li key={item}>{item}</li>)}
+      <button onClick={handleClick}>{contar}</button>
     </div>
   );
 }
