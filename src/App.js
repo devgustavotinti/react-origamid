dac32dec5039ffa4edf7e02e2a6c287aa10155be
Gustavo Tinti <devgustavotinti@gmail.com>
2021-05-20 20:35:32 -0300

    Hooks - Exercicio Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
+import Produto from "./Produto";
 
 
 const App = () => {
-  const [contar, setContar] = React.useState(1)
-  const [itens, setItens] = React.useState(['Item 1'])
+  // Os links abaixo puxam dados de um produto em formato JSON
+  // https://ranekapi.origamid.dev/json/api/produto/tablet
+  // https://ranekapi.origamid.dev/json/api/produto/smartphone
+  // https://ranekapi.origamid.dev/json/api/produto/notebook
+  // Crie uma interface com 3 botões, um para cada produto.
+  // Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+  // Mostre apenas um produto por vez
+  // Mostre a mensagem carregando... enquanto o fetch é realizado
 
-  function handleClick() {
-    setContar((contar) => contar + 1)
-    setItens((itens) => [...itens, 'Item ' + (contar + 1)])
+  const [dados, setDados] = React.useState(null)
+  const [carregando, setCarregando] = React.useState(null)
+
+  async function handleClick(event) {
+    setCarregando(true)
+    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+    const json = await response.json()
+    setDados(json)
+    setCarregando(false)
   }
+
 
   return (
     <div>
-      {itens.map((item) => <li key={item}>{item}</li>)}
-      <button onClick={handleClick}>{contar}</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Notebook</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Smartphone</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Tablet</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 }
