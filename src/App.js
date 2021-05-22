9def9b6941c54fa116e0e934919b750211abc37c
Gustavo Tinti <devgustavotinti@gmail.com>
2021-05-22 09:32:41 -0300

    Hooks - useEffect salvando preferencia no localStorage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from "react";
 import Produto from "./Produto";
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem("produto");
+    if (produtoLocal !== null) setProduto(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) window.localStorage.setItem("produto", produto);
+  }, [produto]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
 
   return (
     <div>
-      {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>
-        {ativo ? "Desativar" : "Ativar"}
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick} style={{ marginRight: "1rem" }}>
+        Notebook
       </button>
+      <button onClick={handleClick}>Smartphone</button>
+      <Produto produto={produto} />
     </div>
   );
 };
